ff31ada5c1564351de52c3346328a69885649ab6
Simon Vandamme <simon.vandamme@skyline.be>
2022-03-02 19:14:49 +0100

    Fixed QAction path
    Change-Id: Ie7287e067484e31a535a893174344316970eda8a

--- QAction_991/QAction_991.cs
+++ QAction_991/QAction_991.cs
@@ -1,30 +1,30 @@
 using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Text;
 
 using Skyline.DataMiner.Scripting;
+using Skyline.Protocol.Streams;
 
 /// <summary>
-/// DataMiner QAction Class.
+/// DataMiner QAction Class: Fill Streams Table.
 /// </summary>
 public static class Streams
 {
-    /// <summary>
-    /// The QAction entry point in case of timeout.
-    /// </summary>
-    /// <param name="protocol">Link with SLProtocol process.</param>
-    public static void ProcessTimeout(SLProtocol protocol)
-    {
-        try
-        {
-
-        }
-        catch (Exception ex)
-        {
-            protocol.Log("QA" + protocol.QActionID + "|" + protocol.GetTriggerParameter() + "|ProcessTimeout|Exception thrown:" + Environment.NewLine + ex, LogType.Error, LogLevel.NoLogging);
-        }
-    }
+	/// <summary>
+	/// The QAction entry point in case of timeout.
+	/// </summary>
+	/// <param name="protocol">Link with SLProtocol process.</param>
+	public static void ProcessTimeout(SLProtocol protocol)
+	{
+		try
+		{
+			StreamsTimeoutProcessor streamsHelper = new StreamsTimeoutProcessor(protocol);
+			streamsHelper.ProcessTimeout();
+			streamsHelper.UpdateProtocol();
+		}
+		catch (Exception ex)
+		{
+			protocol.Log("QA" + protocol.QActionID + "|" + protocol.GetTriggerParameter() + "|ProcessTimeout|Exception thrown:" + Environment.NewLine + ex, LogType.Error, LogLevel.NoLogging);
+		}
+	}
 
 	/// <summary>
 	/// The QAction entry point in case of successful group execution.
@@ -32,13 +32,19 @@ public static class Streams
 	/// <param name="protocol">Link with SLProtocol process.</param>
 	public static void ProcessTable(SLProtocol protocol)
 	{
+		protocol.Log("QA" + protocol.QActionID + "|ProcessTable|### Start of QAction", LogType.DebugInfo, LogLevel.NoLogging);
+
 		try
 		{
-
+			StreamsProcessor streamsHelper = new StreamsProcessor(protocol);
+			streamsHelper.ProcessData();
+			streamsHelper.UpdateProtocol();
 		}
 		catch (Exception ex)
 		{
-			protocol.Log("QA" + protocol.QActionID + "|" + protocol.GetTriggerParameter() + "|ProcessTable|Exception thrown:" + Environment.NewLine + ex, LogType.Error, LogLevel.NoLogging);
+			protocol.Log("QA" + protocol.QActionID + "|" + protocol.GetTriggerParameter() + "|Run|Exception thrown:" + Environment.NewLine + ex, LogType.Error, LogLevel.NoLogging);
 		}
+
+		protocol.Log("QA" + protocol.QActionID + "|ProcessTable|### Start of QAction", LogType.DebugInfo, LogLevel.NoLogging);
 	}
 }
